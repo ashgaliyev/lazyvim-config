cdf76ca1cf2698c5fefca994743abcf502709612
Andrey Ashaliyev <a.ashgaliev@gmail.com>
2023-10-23 20:45:11 +0500

    Add nvim-dap

--- init.lua
+++ init.lua
@@ -19,3 +19,8 @@ vim.cmd([[ autocmd FileType go setlocal noexpandtab tabstop=4 shiftwidth=4 ]])
 vim.api.nvim_command("autocmd BufWritePre *.go :GoFmt")
 vim.api.nvim_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
 
+vim.keymap.set("n", "<F5>", require("dap").continue)
+vim.keymap.set("n", "<F10>", require("dap").step_over)
+vim.keymap.set("n", "<F11>", require("dap").step_into)
+vim.keymap.set("n", "<F12>", require("dap").step_out)
+vim.keymap.set("n", "<leader>b", require("dap").toggle_breakpoint)
